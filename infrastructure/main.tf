b23bc60cb89bea695be1b29556d36017caef0c75
Pixcell <adewergifosse@gmail.com>
2019-07-11 10:04:44 +1200

    WIP: Terraform

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -36,16 +36,48 @@ resource "google_compute_instance" "ecn" {
         sshKeys = "root:${file(var.gce_ssh_pub_key_file)}"
     }
     metadata_startup_script = <<SCRIPT
+curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
 apt-get update
 apt-get -y install nodejs
 apt-get -y install npm
+sudo npm i -g pm2 
+mkdir -p /apps/ecn
 SCRIPT
 
     service_account {
       scopes = ["userinfo-email", "compute-ro", "storage-ro"]
     }
+
+    provisioner "local-exec" {
+      command = "cd ../ && npm i && npm run build"
+    }
+
+    provisioner "file" {
+      source      = "${path.module}/../server"
+      destination = "/apps/ecn/"
+    }
+
+    provisioner "file" {
+      source      = "${path.module}/../build"
+      destination = "/apps/ecn/"
+    }
+
+    provisioner "file" {
+      source      = "${path.module}/../node_modules"
+      destination = "/apps/ecn/"
+    }
+
+    provisioner "remote-exec" {
+      inline = [
+        "cd /apps/ecn/",
+        "PORT=5555 pm2 start server/index.js"
+      ]
+    }
 }
 
-output "ip" {
-  value = "${google_compute_instance.ecn.network_interface.0.access_config.0.nat_ip }"
+output "IP" {
+  value = "${google_compute_instance.ecn.network_interface.0.access_config.0.nat_ip}"
+}
+output "ECN Viewer address" {
+  value = "http://${google_compute_instance.ecn.network_interface.0.access_config.0.nat_ip}:5555/"
 }
