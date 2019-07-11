7c524fdf8db5d14f916bbc47aef4baef57250b34
Rashmi Modhwadia <rashmi.modhwadia@gmail.com>
2019-07-11 16:03:07 +0530

    use ssh key with rsync

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -85,13 +85,8 @@ resource "null_resource" initializeapp {
         destination = "/root/apps/ecn"
     }
 
-    # provisioner "file" {
-    #     source      = "../node_modules"
-    #     destination = "/root/apps/ecn"
-    # } 
-
     provisioner "local-exec" {
-        command = "rsync -e \"ssh -o StrictHostKeyChecking=no\" -avzhr ${path.module}/../node_modules/ root@${google_compute_instance.ecn.network_interface.0.access_config.0.nat_ip}:/root/apps/ecn/"
+        command = "rsync -e \"ssh -o StrictHostKeyChecking=no -i ${var.gce_ssh_private_key_file}" -avzhr ${path.module}/../node_modules/ root@${google_compute_instance.ecn.network_interface.0.access_config.0.nat_ip}:/root/apps/ecn/"
     }
 
     provisioner "remote-exec" {
