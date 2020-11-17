6d6c84c6a850709b94f900cf0b2e953256bc9179
wuwei3 <wei.wu@orange.com>
2020-11-17 12:40:14 +0800

    replace the Map.js and package.json files to test

--- src/ECNViewer/Map.js
+++ src/ECNViewer/Map.js
@@ -1,6 +1,7 @@
 import React from 'react'
 
-import GoogleMapReact from 'google-map-react'
+// import GoogleMapReact from 'google-map-react'
+import { Map as Bmap, APILoader } from '@uiw/react-baidu-map';
 
 import { Badge, Avatar } from '@material-ui/core'
 
@@ -11,6 +12,7 @@ import Icon from '@material-ui/core/Icon'
 import { makeStyles, useTheme } from '@material-ui/styles'
 
 import { statusColor, msvcStatusColor, tagColor } from './utils'
+import { useConfig } from '../providers/Config'
 import { useMap } from '../providers/Map'
 
 const useStyles = makeStyles(theme => ({
@@ -51,6 +53,7 @@ const useStyles = makeStyles(theme => ({
 }))
 
 export default function Map (props) {
+  const { getTagDisplayInfo } = useConfig()
   const classes = useStyles()
   const theme = useTheme()
   const { controller, agent, setAgent, msvcsPerAgent, loading } = props
@@ -58,45 +61,9 @@ export default function Map (props) {
 
   return (
     <div className={classes.mapWrapper} ref={mapRef}>
-      <GoogleMapReact
-        {...map}
-        bootstrapURLKeys={{
-          key: 'AIzaSyChp_fUXiK05ulRl_ewRGKWsQ1k0ULIFkA'
-        }}
-      >
-        {(loading ? [] : controller.agents).filter(a => hasValidCoordinates([a.latitude, a.longitude])).map(a =>
-          <div
-            key={a.uuid}
-            lat={a.latitude} lng={a.longitude}
-            className={classes.mapMarkerTransform}
-            onClick={() => setAgent(a)}
-          >
-            <Badge color='primary' style={{ '--color': msvcStatusColor[a.daemonStatus] }} badgeContent={(msvcsPerAgent[a.uuid] || []).filter(m => m.flowActive && m.status.status === 'RUNNING').length} invisible={a.uuid !== agent.uuid} className={`${classes.msvcBadge}`}>
-              <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-                <Avatar
-                  style={{ '--markerColor': statusColor[a.daemonStatus] }}
-                  className={classes.mapMarker}
-                >
-                  <MemoryIcon />
-                </Avatar>
-                <div style={{ display: 'flex', position: 'absolute', bottom: -15 }}>
-                  {a.tags && a.edgeResources.map(t => t.display ? (t.display.icon ? <div style={{ backgroundColor: t.display.color || tagColor, margin: '2px', padding: '4px', borderRadius: '100%' }}><Icon title={t.display.name || t.name} key={t.display.name || t.name} style={{ fontSize: 16, color: 'white', marginBottom: -3 }}>{t.display.icon}</Icon></div> : null) : null)}
-                </div>
-
-              </div>
-            </Badge>
-          </div>
-        )}
-        {!loading && controller.info && hasValidCoordinates([controller.info.location.lat, controller.info.location.lon]) &&
-          <Avatar
-            lat={controller.info.location.lat}
-            lng={controller.info.location.lon}
-            style={{ '--markerColor': theme.colors.argon }}
-            className={classes.mapMarker}
-          >
-            <CtrlIcon />
-          </Avatar>}
-      </GoogleMapReact>
+      <APILoader akay="49esR3pMIKNz2no4Io15KFWh0P7GSLug">
+        <Bmap />
+      </APILoader>
     </div>
   )
 }
