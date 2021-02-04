e4be9c1c5f603834da2bea626d0ed659e0a803ed
Alex de Wergifosse <alex@edgeworx.io>
2021-02-04 16:58:51 +1300

    Agetn details: Resource utilization

--- src/ECNViewer/AgentDetails/index.js
+++ src/ECNViewer/AgentDetails/index.js
@@ -1,11 +1,12 @@
 import React from 'react'
 
 import ReactJson from 'react-json-view'
-import { Paper, Typography, makeStyles } from '@material-ui/core'
+import { Paper, Typography, makeStyles, Icon } from '@material-ui/core'
 
 import { useData } from '../../providers/Data'
 
 import moment from 'moment'
+import prettyBytes from 'pretty-bytes'
 
 const useStyles = makeStyles(theme => ({
   title: {
@@ -37,9 +38,29 @@ const useStyles = makeStyles(theme => ({
     flexDirection: 'column',
     paddingBottom: '5px'
   },
+  edgeResource: {
+    display: 'flex',
+    alignItems: 'center',
+    paddingBottom: '5px'
+  },
   text: {
     fontSize: '14px',
     fontWeight: 'normal'
+  },
+  erIconContainer: {
+    backgroundColor: `var(--color, ${theme.colors.carbon})`,
+    margin: '2px',
+    padding: '4px',
+    borderRadius: '4px',
+    width: '38px',
+    height: '38px',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  erIcon: {
+    fontSize: 22,
+    color: 'white'
   }
 }))
 
@@ -50,6 +71,8 @@ const _fogTypes = {
 }
 
 const dateFormat = 'YYYY/MM/DD hh:mm:ss a'
+const MiBFactor = 1048576
+const MBFactor = 1000000
 
 export default function AgentDetails ({ views, setView, agent: selectedAgent }) {
   const { data } = useData()
@@ -76,7 +99,7 @@ export default function AgentDetails ({ views, setView, agent: selectedAgent }) 
           <Typography variant='subtitle2' className={classes.title}>Agent Details</Typography>
           <div className={classes.subSection}>
             <span className={classes.subTitle}>Last Active</span>
-            <span className={classes.text}>{moment(agent.lastActive).format(dateFormat)}</span>
+            <span className={classes.text}>{moment(agent.lastStatusTime).format(dateFormat)}</span>
           </div>
           <div className={classes.subSection}>
             <span className={classes.subTitle}>IP Address</span>
@@ -93,9 +116,29 @@ export default function AgentDetails ({ views, setView, agent: selectedAgent }) 
         </div>
         <div className={classes.section}>
           <Typography variant='subtitle2' className={classes.title}>Resource Utilization</Typography>
+          <div className={classes.subSection}>
+            <span className={classes.subTitle}>CPU Usage</span>
+            <span className={classes.text}>{(agent.cpuUsage * 1).toFixed(2) + '%'}</span>
+          </div>
+          <div className={classes.subSection}>
+            <span className={classes.subTitle}>Memory Usage</span>
+            <span className={classes.text}>{`${prettyBytes((agent.memoryUsage * MiBFactor))} / ${prettyBytes((agent.systemAvailableMemory))} (${(agent.memoryUsage * MiBFactor / agent.systemAvailableMemory * 100).toFixed(2)}%)`}</span>
+          </div>
+          <div className={classes.subSection}>
+            <span className={classes.subTitle}>Disk Usage</span>
+            <span className={classes.text}>{`${prettyBytes((agent.diskUsage * MiBFactor))} / ${prettyBytes((agent.systemAvailableDisk))} (${(agent.diskUsage * MiBFactor / agent.systemAvailableDisk * 100).toFixed(2)}%)`}</span>
+          </div>
         </div>
         <div className={classes.section}>
           <Typography variant='subtitle2' className={classes.title}>Edge Resources</Typography>
+          {agent.edgeResources.map(er => (
+            <div key={`${er.name}_${er.version}`} className={classes.edgeResource}>
+              <div className={classes.erIconContainer} style={{ '--color': er.display.color }}>
+                {er.display && er.display.icon && <Icon title={er.display.name || er.name} className={classes.erIcon}>{er.display.icon}</Icon>}
+              </div>
+              <div className={classes.subTitle} style={{ marginLeft: '5px' }}>{(er.display && er.display.name) || er.name} {er.version}</div>
+            </div>
+          ))}
         </div>
       </Paper>
       <Paper className='section'>
