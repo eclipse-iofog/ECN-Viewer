412906effaaf9fcf20e8ea0d9dce4a8703fab609
Alex de Wergifosse <alex@edgeworx.io>
2021-03-17 13:26:04 +1300

    v2.0.0-rc3

--- public/controller-config.js
+++ public/controller-config.js
@@ -1,10 +1,8 @@
 window.controllerConfig = {
-  ip: '34.83.89.255',
   port: '51121',
   refresh: 3000,
-  dev: true,
   user: {
-    email: 'orange@edgeworx.io',
-    password: '0range#1234'
+    email: '',
+    password: ''
   }
 }
